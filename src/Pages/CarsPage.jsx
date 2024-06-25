b858fb938e910a45294efaf9cbf3641df0ef14fe
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2024-06-25 22:16:08 +0500

    1

---
+++ src/Pages/CarsPage.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Cars from "../Components/Cars/Cars"
+export default function CarsPage() {
+  return (
+    <div>
+      <Cars/>
+      
+    </div>
+  )
+}

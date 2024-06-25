5b3f24b5ffca73b538bc9230618888572938b32c
umid2401 <umamatraximov@gmail.com>
2024-06-25 14:09:56 +0500

    car added

--- src/Pages/CarsPages.jsx
+++ src/Pages/CarsPages.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import CarsPage from '../Components/Cars/CarsPage'
+import Cars from '../Components/Cars/Cars'
 export default function CarsPages() {
   return (
     <div>
-      <CarsPage/>
+      <Cars/>
     </div>
   )
 }

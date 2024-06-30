3f7adbeeca882af2b19452ecf7e08a9a9a7accfc
sh-abdurahmonov <sh.abdurahmonv@gmail.com>
2024-06-30 19:41:54 +0500

    about us page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
       colors: {
-        customColor: 'rgb(17, 18, 25)',
-        dropColor:'rgb(39,41,51)',
-         // bu yerda sizning maxsus rangingizni aniqlaymiz
+        customColor: "rgb(17, 18, 25)",
+        dropColor: "rgb(39,41,51)",
+        // bu yerda sizning maxsus rangingizni aniqlaymiz
       },
       backgroundImage: {
-        'custom-gradient': 'linear-gradient(90deg, #3e3d3d, #111219)',
+        "custom-gradient": "linear-gradient(90deg, #3e3d3d, #111219)",
+        "about-us": "./src/assets/about-us-img.avif",
       },
       fontFamily: {
-        lato: ['Lato', 'bold'],
-        thin: ['Lato-Thin'],
-        regular: ['Lato-Regular'],
-
+        lato: ["Lato", "bold"],
+        thin: ["Lato-Thin"],
+        regular: ["Lato-Regular"],
       },
       screens: {
-        'custom': '400px',
+        custom: "400px",
       },
     },
   },
   plugins: [],
-}
-
+};

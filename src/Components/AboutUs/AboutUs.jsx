174afe1c0f85e54e6ecce6fe75e500b36044d384
umid2401 <umamatraximov@gmail.com>
2024-07-04 21:53:50 +0500

    text-fixed

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -9,42 +9,42 @@ const AboutUs = () => {
     <section className="about-us py-10 text-white section">
       <div className="container 2xl:w-[1300px] xl:w-[1250px] lg:w-[950px] md:w-[750px] sm:w-[540px] custom:w-[380px] w-[300px]  mx-auto">
       
-          <h1 className="uppercase text-4xl font-lato mb-8">{t("about")} </h1>
-          <p className="text-white text-2xl font-lato mb-7">
+          <h1 className="uppercase text-white md:text-3xl text-2xl py-2 font-lato">{t("about")} </h1>
+          <p className="text-white  lg:text-[17px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
           {t("welcome")}
           </p>
-          <span className="font-lato text-[18px]">
+          <span className="text-white  lg:text-[18px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
           {t("in-world")}
           </span>
-          <h2 className="text-2xl mt-10 mb-5 font-lato">
+          <h2 className="text-white md:text-3xl text-2xl py-2 font-lato">
           {t("why")}
           </h2>
-          <p className="text-[18px] mb mb-7 font-lato">
-            <span className="text-2xl ">{t("elegant")}</span> 
+          <p className="text-white  lg:text-[18px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
+            <span className="text-[16px] font-regular ">{t("elegant")}</span> 
             {t("provide")}
           </p>
-          <p className="text-[18px] mb-7 font-lato">
-            <span className="text-2xl">{t("worry")}</span>
+          <p className="text-white  lg:text-[18px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
+            <span className="text-[16px] lg:text-[18px] font-regular">{t("worry")}</span>
             {t("our-goal")}
           </p>
-          <p className="text-[18px] mb-7 font-lato">
-            <span className="text-2xl">{t("trust")}</span>
+          <p className="text-white  lg:text-[18px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
+            <span className="text-[16px] lg:text-[18px] font-regular">{t("trust")}</span>
             {t("we-value")}
           </p>
-          <p className="text-[18px] mb-16 font-lato">
-            <span className="text-2xl">{t("service_at")}</span>
+          <p className="text-white  lg:text-[18px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
+            <span className="text-[16px] lg:text-[18px] font-regular">{t("service_at")}</span>
             {t("our-friendly")}
           </p>
-          <h2 className="text-2xl mt-10 mb-5 font-lato">
+          <h2 className="text-white md:text-3xl text-2xl py-2 font-lato">
           {t("our-miision")}
           </h2>
-          <span className="text-2xl mb-16 font-lato">
+          <span className="text-white  lg:text-[18px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
           {t("a-mission")}
           </span>
-          <h2 className="text-2xl mt-10 mb-5 font-lato">
+          <h2 className="text-white md:text-3xl text-2xl py-2 font-lato">
           {t("contact-us")}
           </h2>
-          <span className="text-sm mb-7 font-lato">
+          <span className="text-white  lg:text-[18px] lg:leading-9 text-[14px] leading-7 py-2 font-lato">
           {t("c-trust")}
           </span>
       </div>

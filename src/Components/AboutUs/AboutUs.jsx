2a096aba8eb9894dffe3237b59a2178920a030c5
sh-abdurahmonov <sh.abdurahmonv@gmail.com>
2024-07-04 20:00:54 +0500

    fixed styles

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,56 +1,62 @@
 import "./AboutUs.css";
 const AboutUs = () => {
   return (
-    <section className="about-us ">
-      <div className=" py-12 container 2xl:w-[1300px] xl:w-[1120px] lg:w-[950px] md:w-[750px] sm:w-[540px] custom:w-[380px] w-[300px]  mx-auto">
-        <h1 className="uppercase text-white mb-8 ">about us</h1>
-        <p className="text-white text-2xl mb-7">
+    <section className="about-us">
+      <div className="py-12 container 2xl:w-[1300px] xl:w-[1120px] lg:w-[950px] md:w-[750px] sm:w-[540px] custom:w-[380px] w-[300px] mx-auto">
+        <h1 className="uppercase text-white mb-20 font-bold text-4xl md:text-2xl sm:text-lg">
+          about us
+        </h1>
+        <p className="text-white text-3xl md:text-xl sm:text-base mb-9">
           Welcome to Auto Zoom Car Rental Co LLC – Your Partner in Unforgettable
           Journeys in Dubai!
         </p>
-        <span className="text-sm text-white">
+        <span className="text-base text-white mb-9">
           In a world where every trip is an adventure, Auto Zoom Car Rental Co
           LLC offers you a unique car rental experience in the heart of Dubai.
         </span>
-        <h2 className="capitalize text-2xl text-white mb-7 mt-5">
+        <h2 className="capitalize text-4xl font-bold text-white mb-7 mt-5">
           why auto zoom
         </h2>
-        <p className="text-white text-sm mb mb-7">
-          <span className="text-xl">Elegant Car Fleet:</span> We provide you the
-          opportunity to choose from our luxurious car fleet – from sleek sedans
-          to powerful SUVs. We have a vehicle to match your style and meet your
-          needs.
+        <p className="text-white text-base mb mb-7">
+          <span className="text-2xl font-semibold">Elegant Car Fleet: </span> We
+          provide you the opportunity to choose from our luxurious car fleet –
+          from sleek sedans to powerful SUVs. We have a vehicle to match your
+          style and meet your needs.
         </p>
-        <p className="text-white text-sm mb-7">
-          <span className="text-xl">Worry-Free Rental:</span>Our goal is to make
-          your journey as comfortable as possible. Flexible rental terms, the
-          option to hire with a driver, and a wide range of additional services
-          – all for your convenience.
+        <p className="text-white text-base mb-7">
+          <span className="text-2xl font-semibold">Worry-Free Rental: </span>{" "}
+          Our goal is to make your journey as comfortable as possible. Flexible
+          rental terms, the option to hire with a driver, and a wide range of
+          additional services – all for your convenience.
         </p>
-        <p className="text-white text-sm mb-7">
-          <span className="text-xl">Transparency and Trust:</span>We value every
-          customer, so our prices are honest and transparent. No hidden fees, no
-          unexpected surprises. Trust us, and we will make your rental
-          experience straightforward and clear.
+        <p className="text-white text-base mb-7">
+          <span className="text-2xl font-semibold">
+            Transparency and Trust:
+          </span>{" "}
+          We value every customer, so our prices are honest and transparent. No
+          hidden fees, no unexpected surprises. Trust us, and we will make your
+          rental experience straightforward and clear.
         </p>
-        <p className="text-white text-sm mb-16">
-          <span className="text-xl">Service at Your Level: </span>Our friendly
-          and professional team is ready to bring your ideas to life. We aim not
-          just to provide a car but to create a genuine automotive experience
-          for you.
+        <p className="text-white text-base mb-16">
+          <span className="text-2xl font-semibold">
+            Service at Your Level:{" "}
+          </span>
+          Our friendly and professional team is ready to bring your ideas to
+          life. We aim not just to provide a car but to create a genuine
+          automotive experience for you.
         </p>
-        <h2 className="capitalize text-2xl text-white mb-7 mt-5">
+        <h2 className="capitalize text-4xl font-bold text-white mb-7 mt-5">
           Our Mission:
         </h2>
-        <span className="text-sm text-white mb-7">
+        <span className="text-base text-white mb-7">
           Auto Zoom Car Rental Co LLC is created with one goal – to make every
           journey unforgettable. We strive to offer you not just a car but the
           key to new experiences and adventures.
         </span>
-        <h2 className="capitalize text-2xl text-white mb-7 mt-5">
+        <h2 className="capitalize text-4xl font-bold text-white mb-7 mt-5">
           contact us:
         </h2>
-        <span className="text-sm text-white mb-7">
+        <span className="text-base text-white mb-7">
           Trust the professionals – trust Auto Zoom Car Rental Co LLC. Get in
           touch with us today to start your exciting journey through Dubai in
           our stylish and reliable cars. Allow yourself to savor every moment of
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,24 +1,24 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import Home_slider from '../Components/Home_slider/Home_slider'
-import Home_card_sliders from '../Components/Home_slider/Home_card_sliders'
-import Home_cars_data_carousel from '../Components/Home_slider/Home_cars_data_carousel'
-import Home_Content from '../Components/Home/Home_Content'
-import Home_Video from "../Components/Home/Home_Video"
-import Faq from "../Components/Home/Faq"
-import Services from "../Components/Services/Services"
-import FollowUs from '../Components/Home/FollowUs'
+import React from "react";
+import Home_slider from "../Components/Home_slider/Home_slider";
+import Home_card_sliders from "../Components/Home_slider/Home_card_sliders";
+import Home_cars_data_carousel from "../Components/Home_slider/Home_cars_data_carousel";
+import Home_Content from "../Components/Home/Home_Content";
+import Home_Video from "../Components/Home/Home_Video";
+import Faq from "../Components/Home/Faq";
+import Services from "../Components/Services/Services";
+import FollowUs from "../Components/Home/FollowUs";
 export default function HomePage() {
   return (
-    <div>
-      <Home_slider/>
-      <Home_card_sliders/>
-      <Home_cars_data_carousel/>
-      <Services/>
-      <Home_Content/>
-      <Home_Video/>
-      <Faq/>
-      <FollowUs/>
-    </div>
+    <main className="grow">
+      <Home_slider />
+      <Home_card_sliders />
+      <Home_cars_data_carousel />
+      <Services />
+      <Home_Content />
+      <Home_Video />
+      <Faq />
+      <FollowUs />
+    </main>
   );
 }
